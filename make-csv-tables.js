de4de80bc1d1592e3b3b8e2cee73df8745fcc6e3
Jason Kridner <jdk@ti.com>
2019-09-17 18:00:38 -0400

    Remove intermediate JSON file

--- make-csv-tables.js
+++ make-csv-tables.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
+var fs = require("fs");
 var csv = require("fast-csv");
-var table = require("./BeagleBone_Cape_Header_Pins.json");
+var table1 = [];
 
-var csvStream = csv.format({ headers: true, quoteHeaders: true });
-csvStream.pipe(process.stdout);
-for(var i in table) {
-  csvStream.write(table[i]);
+function parseInput(table, callback) {
+  var csvParse = csv.parse({ headers: true });
+  var csvStreamIn = fs.createReadStream("BeagleBone_Cape_Header_Pins.csv");
+  var parsedStream = csvStreamIn.pipe(csvParse);
+  parsedStream.on("error", function(error) {
+    console.error(error);
+  });
+  parsedStream.on("data", function(row) {
+    table.push(row);
+  });
+  parsedStream.on("end", function(rowCount) {
+    callback(table, rowCount);
+  });
 }
-process.stdout.write("\n");
+
+function formatOutput(table, rowCount) {
+  //console.error("Parsed " + rowCount + " rows");
+  var csvStreamOut = csv.format({ headers: true, quoteHeaders: true });
+  csvStreamOut.pipe(process.stdout);
+  for(var i in table) {
+    csvStreamOut.write(table[i]);
+  }
+  process.stdout.write("\n");
+}
+
+parseInput(table1, formatOutput);
